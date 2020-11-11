c39e1af5969b0519ccdf120e42883b905d0c9ed4
Jamie Carruthers <jamie@otixo.com>
2020-11-11 11:03:42 +0100

    assert the button has a loading class after submitting the form

--- examples/rest-react/e2e/Login.e2e.test.js
+++ examples/rest-react/e2e/Login.e2e.test.js
@@ -4,23 +4,26 @@ describe('Login', () => {
   })
 
   describe('given I logged in using existing user', () => {
-    before(() => {
-      cy.get('#username').type('johnUser{enter}')
-    })
 
-    it('should display my user ID', () => {
+    
+    it.only('should display my user ID', () => {
+      cy.get('[data-testid=button]').should('not.have.class', 'loading')
+      cy.get('#username').type('johnUser{enter}')
+      /* 
+        at this point the form has been submitted and the 
+        button should have a loading class and the loading text should be visible
+      */
+      cy.get('[data-testid=loading]').should('be.visible')
+
+      /*
+        For some reason Cypress does not find the button as the app has already
+        rendered the logged in state, despite there being a 2 second delay on the login handler
+      */
+      cy.get('[data-testid=button]').should('have.class', 'loading')
       cy.get('[data-testid="userId"').should(
         'have.text',
         'f79e82e8-c34a-4dc7-a49e-9fadc0979fda'
-      )
-    })
-
-    it('should display my first name', () => {
-      cy.get('[data-testid="firstName"]').should('have.text', 'John')
-    })
-
-    it('should display my username', () => {
-      cy.get('[data-testid="lastName"]').should('have.text', 'Maverick')
+        )
     })
   })
 })
--- examples/rest-react/src/LoginForm.js
+++ examples/rest-react/src/LoginForm.js
@@ -1,50 +1,52 @@
-import React, { useState, useCallback } from 'react'
+import React, { useState, useCallback } from "react";
 
 export const LoginForm = () => {
   // Store the username so we can reference it in a submit handler
-  const [username, setUsername] = useState('')
+  const [username, setUsername] = useState("");
 
   // Create a state for the user data we are going to receive
   // from the API call upon form submit.
-  const [userData, setUserData] = useState(null)
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   // Whenever we change our username input's value
   // update the corresponding state's value.
   const handleUsernameChange = useCallback((event) => {
-    setUsername(event.target.value)
-  }, [])
+    setUsername(event.target.value);
+  }, []);
 
   // Handle a submit event of the form
   const handleFormSubmit = useCallback(
     (event) => {
       // Prevent the default behavior, as we don't want
       // for our page to reload upon submit.
-      event.preventDefault()
+      event.preventDefault();
+      setLoading(true);
 
       // Perform a POST /login request and send the username
-      fetch('/login', {
-        method: 'POST',
+      fetch("/login", {
+        method: "POST",
         body: JSON.stringify({
           username,
         }),
       })
         .then((res) => res.json())
         // Update the state with the received response
-        .then(setUserData)
+        .then(setUserData);
     },
     [username]
-  )
+  );
 
   if (userData) {
     return (
       <div>
         <h1>
-          <span data-testid="firstName">{userData.firstName}</span>{' '}
+          <span data-testid="firstName">{userData.firstName}</span>{" "}
           <span data-testid="lastName">{userData.lastName}</span>
         </h1>
         <p data-testid="userId">{userData.id}</p>
       </div>
-    )
+    );
   }
 
   return (
@@ -57,8 +59,11 @@ export const LoginForm = () => {
           value={username}
           onChange={handleUsernameChange}
         />
-        <button type="submit">Submit</button>
+        {loading && <div data-testid="loading">loading</div>}
+        <button type="submit" className={loading ? "loading" : ""}>
+          Submit
+        </button>
       </div>
     </form>
-  )
-}
+  );
+};
