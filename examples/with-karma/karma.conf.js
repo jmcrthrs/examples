b64434f384d634b8e6e1e739cd329ee18a701e54
Artem Zakharchenko <kettanaito@gmail.com>
2020-11-11 10:25:34 +0100

    Configures the "with-karma" example

---
+++ examples/with-karma/karma.conf.js
@@ -0,0 +1,31 @@
+'use strict'
+
+module.exports = function (config) {
+  config.set({
+    basePath: './test',
+    frameworks: ['mocha', 'chai'],
+    files: ['*.js'],
+    proxies: {
+      '/mockServiceWorker.js': '/base/mockServiceWorker.js',
+    },
+    exclude: [],
+    preprocessors: {
+      '*.js': ['webpack'],
+    },
+    webpack: {
+      mode: 'development',
+    },
+    reporters: ['mocha'],
+    plugins: [
+      'karma-chai',
+      'karma-chrome-launcher',
+      'karma-mocha',
+      'karma-mocha-reporter',
+      'karma-webpack',
+    ],
+    port: 9876,
+    colors: true,
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+  })
+}
